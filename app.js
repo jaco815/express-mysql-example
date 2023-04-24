ecbcc3bef0d37f1ec96fb4b368ce2898d2643a68
DESKTOP-K5B4R34\admin <djaco@naver.com>
2023-04-24 13:47:34 +0900

    4.24

--- app.js
+++ app.js
@@ -14,36 +14,36 @@ app.get('/',function(req,res){
 })
 
 
-// app.get('/board/write',function(req,res){
-//    res.render('write')
-// })
+app.get('/board/write',function(req,res){
+   res.render('write')
+})
 
-// app.post('/board/write',function(req,res){
-//    db.query('INSERT INTO board (title, content) VALUES (?,?)',[req.body.title,req.body.content],function(err,rows){
-//       if(err)
-//          console.log(err)
-//       else
-//          res.send('success')
-//    })
-// })
+app.post('/board/write',function(req,res){
+   db.query('INSERT INTO board (title, content) VALUES (?,?)',[req.body.title,req.body.content],function(err,rows){
+      if(err)
+         console.log(err)
+      else
+         res.send('success')
+   })
+})
 
-// app.put('/board/update',function(req,res){
-//    db.query('UPDATE board SET title=?, content=? WHERE id=?',[req.body.title,req.body.content,req.body.id],function(err,rows){
-//       if(err)
-//          console.log(err)
-//       else
-//          res.send('success')
-//    })
-// })
+app.put('/board/update',function(req,res){
+   db.query('UPDATE board SET title=?, content=? WHERE id=?',[req.body.title,req.body.content,req.body.id],function(err,rows){
+      if(err)
+         console.log(err)
+      else
+         res.send('success')
+   })
+})
 
-// app.delete('/board/delete',function(req,res){
-//    db.query('DELETE FROM board WHERE id=?',[req.body.id],function(err,rows){
-//       if(err)
-//          console.log(err)
-//       else
-//          res.send('success')
-//    })
-// })
+app.delete('/board/delete',function(req,res){
+   db.query('DELETE FROM board WHERE id=?',[req.body.id],function(err,rows){
+      if(err)
+         console.log(err)
+      else
+         res.send('success')
+   })
+})
 
 app.get('/board',function(req,res){
    db.query('SELECT * FROM board',function(err,rows){
